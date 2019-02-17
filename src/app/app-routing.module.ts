adacd44c860dee9cfc2bf1c29a842f7b2c1e4cca
hoummani <yassinehoummani.tsi@gmail.com>
2019-02-17 18:02:50 +0100

    login and register services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
 
 const routes: Routes = [
   {path: '' , component: WelcomeComponent,   pathMatch: 'full'},
-  {path: '**' , component: WelcomeComponent}
+  {path: 'login' , component: LoginComponent},
+  {path: 'register' , component: RegisterComponent}
 ];
 
 @NgModule({
